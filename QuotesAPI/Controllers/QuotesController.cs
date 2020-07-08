44b7526292c14bf521620ed9e4813cefc37f499f
Gaurav Balyan <gaurav.balyan24@gmail.com>
2020-07-08 21:30:53 +0530

    first commit

---
+++ QuotesAPI/Controllers/QuotesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuotesAPI.Data;
+using QuotesAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuotesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class QuotesController : ControllerBase
+    {
+        private QuotesDbContext _quotesDbContext;
+
+        public QuotesController(QuotesDbContext quotesDbContext) {
+            _quotesDbContext = quotesDbContext;
+        }
+
+        // GET: api/<QuotesController>
+        [HttpGet]
+        [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Client)]
+        [AllowAnonymous]
+        public IActionResult Get(string sort)
+        {
+            IQueryable<Quote> quotes;
+            switch (sort) {
+                case "desc":
+                    quotes = _quotesDbContext.Quotes.OrderByDescending(q=> q.CreatedAt);
+                    break;
+                case "asc":
+                    quotes = _quotesDbContext.Quotes.OrderBy(q=> q.CreatedAt);
+                    break;
+                default:
+                    quotes = _quotesDbContext.Quotes;
+                    break;
+            }
+            return Ok(quotes);
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult PagingQuote(int? pageNumber, int? pageSize) {
+            var quotes = _quotesDbContext.Quotes;
+            var currentPageNumber = pageNumber ?? 1;
+            var currentPageSize = pageSize ?? 5;
+            return Ok(quotes.Skip((currentPageNumber - 1) * currentPageSize).Take(currentPageSize));
+        }
+
+        [HttpGet]
+        [Route("[action]")] 
+        public IActionResult SearchQuote(string type) {
+            var quotes = _quotesDbContext.Quotes.Where(q=> q.Type.StartsWith(type));
+            return Ok(quotes);
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult MyQuote() {
+            string userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            var quotes = _quotesDbContext.Quotes.Where(q=> q.UserId == userId);
+            return Ok(quotes);
+        }
+
+        // GET api/<QuotesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var quote = _quotesDbContext.Quotes.Find(id);
+            if (quote == null) {
+                return NotFound("No record found...");
+            }
+            return Ok(quote);
+        }
+
+        // POST api/<QuotesController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Quote quote)
+        {
+            string userId = User.Claims.FirstOrDefault(c=> c.Type == ClaimTypes.NameIdentifier).Value;
+            quote.UserId = userId;
+            quote.CreatedAt = DateTime.Now;
+            _quotesDbContext.Quotes.Add(quote);
+            _quotesDbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        // PUT api/<QuotesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Quote quote)
+        {
+            string userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            var entity = _quotesDbContext.Quotes.Find(id);
+            if (entity == null)
+            {
+                return NotFound("No record found against this id...");
+            }
+            if (userId != entity.UserId) {
+                return BadRequest("You are not authorized to update this record");
+            }
+            else {
+                entity.Title = quote.Title;
+                entity.Author = quote.Author;
+                entity.Description = quote.Description;
+                entity.Type = quote.Type;
+                entity.CreatedAt = DateTime.Now;
+                _quotesDbContext.SaveChanges();
+                return Ok("Record updated successfully");
+            }
+        }
+
+        // DELETE api/<QuotesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            string userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            var quote = _quotesDbContext.Quotes.Find(id);
+            if (quote == null)
+            {
+                return NotFound("No record found against this id...");
+            }
+            if (userId != quote.UserId)
+            {
+                return BadRequest("You are not authorized to delete this record");
+            }
+            else {
+                _quotesDbContext.Quotes.Remove(quote);
+                _quotesDbContext.SaveChanges();
+                return Ok("Quote deleted...");
+            }
+        }
+    }
+}
